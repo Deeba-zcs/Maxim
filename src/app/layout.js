9f1122e3e88f197d7818836c3e40e8c717716696
Deeba-zcs <deeba_zuberi@codenticsoftware.com>
2023-06-05 13:33:22 +0530

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,12 @@ export default function RootLayout({ children }) {
   return (
   
     <html lang="en">
+      <head>
+   <link  href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css"/>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+   </head>
       <body className={inter.className}>
 
       <Header />
